0f4acdcb3e98fac06c7f3b9cd2fa950cf4e58d60
fleeingdolphin <79707157+fleeingdolphin@users.noreply.github.com>
2021-02-26 17:32:16 -0300

    querying the oracle, price feeds only enabled by the owner

--- contracts/protocol/DIAPriceOracle.sol
+++ contracts/protocol/DIAPriceOracle.sol
@@ -1,23 +1,5 @@
-
-/*
-    Copyright 2020 Protofire
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-    SPDX-License-Identifier: Apache License, Version 2.0
-*/
-
 pragma solidity 0.6.10;
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import {DIAOracle} from "../../external/contracts/DIAOracle.sol";
 
 /**
@@ -27,10 +9,11 @@ import {DIAOracle} from "../../external/contracts/DIAOracle.sol";
  * calculated using common asset pairs, or uses external data to calculate price.
  * Note: Prices are returned in preciseUnits (i.e. 18 decimals of precision)
  */
-contract DIAPriceOracle {
+contract DIAPriceOracle is Ownable {
     // Token address of the bridge asset that prices are derived from if the specified pair price is missing, required by the interface
     address public immutable masterQuoteAsset;
     DIAOracle public immutable underlyingOracle;
+    mapping (address => mapping (address => string)) private  priceIdentifiers;
 
     constructor( address _masterQuoteAsset, address _underlyingOracle) public {
       masterQuoteAsset =_masterQuoteAsset;
@@ -40,5 +23,18 @@ contract DIAPriceOracle {
     /* ============ External Functions ============ */
     // required by the interface
     function getPrice(address _assetOne, address _assetTwo) external view returns (uint256) {
+        uint256 price;
+        (price,,,) = underlyingOracle.getCoinInfo(getPriceIdentifier(_assetOne,_assetTwo));
+        return price;
+    }
+
+    function addPair(address _assetOne, address _assetTwo, string memory identifier) onlyOwner external {
+      priceIdentifiers[_assetOne][_assetTwo] = identifier;
+    }
+
+    function getPriceIdentifier (address _assetOne, address _assetTwo) public view returns (string memory identifier){
+        identifier = priceIdentifiers[_assetOne][_assetTwo];
+        require(bytes(identifier).length != 0,"Price feed not available");
+        return identifier;
     }
 }
--- test/protocol/diaPriceOracle.spec.ts
+++ test/protocol/diaPriceOracle.spec.ts
@@ -1,6 +1,7 @@
 import "module-alias/register";
 
 import { Account } from "@utils/test/types";
+import { BigNumber } from "@ethersproject/bignumber";
 import { DIAPriceOracle, DIAOracle } from "@utils/contracts";
 import DeployHelper from "@utils/deploys";
 import {
@@ -8,6 +9,7 @@ import {
   getAccounts,
   getWaffleExpect
 } from "@utils/test/index";
+import { ether } from "@utils/index";
 
 const expect = getWaffleExpect();
 
@@ -15,6 +17,9 @@ describe.only("DIAPriceOracle", () => {
   let wallet: Account;
 
   let usdc: Account;
+  let weth: Account;
+  let unrelatedUser: Account;
+  const identifier: string = "ETH/USD";
 
   let diaOracle: DIAOracle;
   let diaPriceOracle: DIAPriceOracle;
@@ -22,10 +27,13 @@ describe.only("DIAPriceOracle", () => {
   let deployer: DeployHelper;
 
   addSnapshotBeforeRestoreAfterEach();
-
-  before(async () => {
+  const configure = async () => {
     // Using this syntax for sol-coverage to work
-    [wallet, usdc ] = await getAccounts();
+    [wallet, usdc, weth, unrelatedUser ] = await getAccounts();
+
+    console.log(unrelatedUser);
+    console.log(unrelatedUser.wallet);
+    console.log(Object.keys(unrelatedUser));
 
     deployer = new DeployHelper(wallet.wallet);
 
@@ -35,14 +43,46 @@ describe.only("DIAPriceOracle", () => {
       usdc.address,
       diaOracle.address
     );
-  });
+  };
 
   describe("constructor", async () => {
+    before(configure);
     it("should have the masterQuoteAsset configured", async () => {
       expect(await diaPriceOracle.masterQuoteAsset()).to.eq(usdc.address);
     });
     it("should have the underlyingOracle configured", async () => {
       expect(await diaPriceOracle.underlyingOracle()).to.eq(diaOracle.address);
+    });
+  });
+
+  describe("addPair", async () => {
+    before(configure);
+    const ethPrice: BigNumber = ether(1500);
+    it(
+      "WHEN there is no price feed configured, THEN it should revert",
+       () => expect(diaPriceOracle.getPrice(weth.address, usdc.address)).to.be.revertedWith("Price feed not available")
+      );
+
+    describe("GIVEN a valid price feed", () => {
+      before(() => diaOracle.updateCoinInfo(identifier, identifier, ethPrice, 0, Date.now().toString()));
+    describe("WHEN adding a pair to a DiaPriceOracle", () => {
+      before(() =>  diaPriceOracle.addPair(weth.address, usdc.address, identifier) );
+      it("THEN the pricefeed identifier can be retrieved", async() => {
+        expect(await diaPriceOracle.getPriceIdentifier(weth.address, usdc.address)).to.eq(identifier);
+      });
+      it("AND the price value can be retrieved", async() => {
+        expect(await diaPriceOracle.getPrice(weth.address, usdc.address)).to.eq(ethPrice);
+      });
+    });
+    });
+  });
+
+  describe("permissions", async () => {
+    it( "Only the contract admin should be able to add a paie", async() => {
+      const connectedDiaPriceOracle: DIAPriceOracle = diaPriceOracle.connect(unrelatedUser.wallet);
+      await expect(connectedDiaPriceOracle.addPair(weth.address, usdc.address, identifier)).to.be.revertedWith("Ownable: caller is not the owner"); }
+      );
+    it.skip("Only the contract admin should be able to remove a pair", async () => {
     });
   });
 
